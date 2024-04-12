dbaece574f539aad3fad5df0ae198d6f2faf2fc8
r.dain24 <r.dain24@LAB108PC12>
2024-04-12 12:48:47 +0300

    yayyyyyy

--- MiniProject1/Form1.cs
+++ MiniProject1/Form1.cs
@@ -1,3 +1,10 @@
+using System.Data.SqlClient;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using System.Windows.Forms;
+using System.Data;
+using System.Text;
+using System.Security.Cryptography;
+
 namespace MiniProject1
 {
     public partial class Form1 : Form
@@ -19,16 +26,36 @@ namespace MiniProject1
                 Form2 steve = new Form2(true);
                 steve.Show();
             }
-            else if (username.Contains("user") && password.Contains("user"))
-            {
-                IsAdmin = false;
-                DialogResult = DialogResult.OK;
-                Form2 steve = new Form2(false);
-                steve.Show();
-            }
             else
             {
-                MessageBox.Show("enter valid credentials");
+                using (SqlConnection con = new SqlConnection(@"Data Source=LAB108PC12\SQLEXPRESS;Initial Catalog=Tourism;Integrated Security=True"))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT 1 FROM users WHERE username = @username", con);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.SelectCommand = cmd;
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        string righthash = (string)dt.Rows[0]["PswdHash"];
+                        string salt = (string)dt.Rows[0]["TimeUser"];
+                        string userhash = hashPassword($"{password}{salt}");
+                        if(righthash == userhash)
+                        {
+                            IsAdmin = false;
+                            DialogResult = DialogResult.OK;
+                            Form2 steve = new Form2(false);
+                            steve.Show();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username not found");
+                    }
+                    con.Close();
+                }
             }
         }
 
@@ -37,6 +64,14 @@ namespace MiniProject1
             DialogResult = DialogResult.OK;
             Form4 steve = new Form4();
             steve.Show();
+        }
+
+        string hashPassword(string password)
+        {
+            SHA256 hashAlgorithm = SHA256.Create();
+            var bytes = Encoding.Default.GetBytes(password);
+            var hash = hashAlgorithm.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
         }
     }
 }
